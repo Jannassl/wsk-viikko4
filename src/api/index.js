e2fb76c325422e4c73584496243573d097fe779b
Jannassl <Jan.nassling@metropolia.fi>
2024-04-12 10:51:38 +0300

    Assignment2 valmis

---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import express from 'express';
+import catRouter from './routes/cat-router.js';
+import userRouter from './routes/user-router.js';
+
+const router = express.Router();
+
+// bind base url for all cat routes to catRouter
+router.use('/user', userRouter);
+router.use('/cat', catRouter)
+
+export default router;
--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 'use strict'
 
 import express from 'express';
-const hostname = '127.0.0.1';
+import api from './api/index.js';
+
 const app = express();
-const port = 3000;
+
 
 app.use('/public', express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/api/v1', api);
 
 app.get('/', (req, res) => {
   res.send('Welcome to my REST API!');
-});
-
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 app.get('/api/v1/cat', (req,res) => {
@@ -27,3 +27,5 @@ const cat = {
     owner : "Heikki Lamminen",
     Image : "https://loremflickr.com/320/240/cat"
 }
+
+export default app;
